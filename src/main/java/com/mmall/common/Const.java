6d467f642e9cef4d5ea59e24e9e81948e8b306a3
Eddy <782303268@qq.com>
2022-05-27 21:01:16 +0800

    Chap 11 Order finished, and all chaps finished. So this is a final version.

--- src/main/java/com/mmall/common/Const.java
+++ src/main/java/com/mmall/common/Const.java
@@ -79,6 +79,15 @@ public class Const {
         public void setCode(int code) {
             this.code = code;
         }
+
+        public static OrderStatusEnum codeOf(int code){
+            for(OrderStatusEnum orderStatusEnum : values()){
+                if(orderStatusEnum.getCode() == code){
+                    return orderStatusEnum;
+                }
+            }
+            throw new RuntimeException("么有找到对应的枚举");
+        }
     }
 
     public interface  AlipayCallback{
@@ -106,5 +115,35 @@ public class Const {
         public int getCode() {
             return code;
         }
+    }
+
+    public enum PaymentTypeEnum{
+        ONLINE_PAY(1,"在线支付");
+
+        PaymentTypeEnum(int code,String value){
+            this.code = code;
+            this.value = value;
+        }
+        private String value;
+        private int code;
+
+        public String getValue() {
+            return value;
+        }
+
+        public int getCode() {
+            return code;
+        }
+
+
+        public static PaymentTypeEnum codeOf(int code){
+            for(PaymentTypeEnum paymentTypeEnum : values()){
+                if(paymentTypeEnum.getCode() == code){
+                    return paymentTypeEnum;
+                }
+            }
+            throw new RuntimeException("么有找到对应的枚举");
+        }
+
     }
 }
